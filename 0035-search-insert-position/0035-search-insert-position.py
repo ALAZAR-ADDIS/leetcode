1aa842ac81aaf96c2b2c1de3ab27634ea2559fe0
ALAZAR-ADDIS <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-04-06 13:44:31 -0700

    Time: 52 ms (39.00%), Space: 17.4 MB (49.19%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,8 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        rightbound=len(nums)-1
-        leftbound=0
-        while True:
+        
+        '''while True:
             mid=(rightbound+leftbound)//2
             if nums[mid]==target:
                 return mid
@@ -16,4 +15,21 @@ class Solution:
             else:
                 leftbound=mid+1
                 if leftbound>rightbound:
-                    return mid+1
+                    return mid+1'''
+        rightbound=len(nums)-1
+        leftbound=0
+        while True:
+            mid=(rightbound+leftbound)//2
+            if nums[mid]==target:
+                return mid
+            elif target<nums[mid]:
+                rightbound=mid-1
+            else:
+                leftbound=mid+1
+
+            if leftbound==len(nums):
+                return len(nums)
+            elif rightbound<leftbound:
+                return rightbound+1       
+        
+        
