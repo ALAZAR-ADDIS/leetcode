ce6b9150b4982e75f5b32aa29ebf3fcdd67eae7f
ALAZAR-ADDIS <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-07-06 01:21:45 -0700

    Time: 485 ms (28.90%), Space: 30.3 MB (67.57%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        ''' #approch 1
         index=0
         while index<len(nums):
             correct=nums[index]-1
@@ -8,6 +9,16 @@ class Solution:
             else:
                 if index!=correct :
                     return nums[correct]
+                index+=1'''
+        index=0
+        while index<len(nums):
+            correct=nums[index]-1
+            if index!=correct:
+                if nums[index]!=nums[correct]:
+                    nums[index],nums[correct]=nums[correct],nums[index]
+                else:
+                    return nums[index]
+            else:
                 index+=1
        
         
