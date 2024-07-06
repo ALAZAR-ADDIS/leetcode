1496354bcdf964987eea1787df4003346f5d203b
ALAZAR-ADDIS <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-07-06 00:38:28 -0700

    Time: 293 ms (8.45%), Space: 24.5 MB (95.91%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,20 +1,21 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        #cycle sort algorithm
         index=0
-        while(index<len(nums)):
+        while index<len(nums):
             correct=nums[index]-1
-            if  nums[index]!=nums[correct]:
-                nums[correct],nums[index]=nums[index],nums[correct]
+            if nums[index]!=nums[correct]:
+                nums[index],nums[correct]=nums[correct],nums[index]
             else:
-                  index+=1
-                
-        missingList=[]
-        count=0
-        for i in range(0,len(nums)):
-            if nums[i]!=i+1:
-                missingList.append(i+1)
-                
-        return missingList
+                index+=1
+        
+        #find the duplicates
+        duplicateList=[]
+        for i in range(len(nums)):
+            if nums[i]-1!=i:
+                  duplicateList.append(i+1)
+        return duplicateList
+        
 
 
 
