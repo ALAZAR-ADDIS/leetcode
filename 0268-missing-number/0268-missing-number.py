2f81830d4790fac48a4a439f1a205e59e7e74f38
ALAZAR-ADDIS <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-07-05 23:59:45 -0700

    Time: 124 ms (22.55%), Space: 17.8 MB (20.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,19 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #return the number which is duplicated
+        #cycle sort
+
+        #sortin algorithm 
         index=0
         while index<len(nums):
             correct=nums[index]
-            if correct<len(nums) and nums[correct]!=nums[index]   :
-                 nums[correct],nums[index]=nums[index],nums[correct]
+            if correct<len(nums) and nums[index]!=nums[correct]:
+                nums[index],nums[correct]=nums[correct],nums[index]
             else:
                 index+=1
-               
-        count=0
-        while count<len(nums):
-            if nums[count]!=count:
-                return count
-            count+=1
+
+        #find the duplicates
+        for i in range(len(nums)):
+            if nums[i]!=i:
+                return i
         return len(nums)
-        
-       
-        
+
+      
