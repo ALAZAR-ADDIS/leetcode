d5ade0079c56f30de9a7fdaabdc1d65d737758ef
ALAZAR-ADDIS <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-09-25 03:32:26 -0700

    Time: 38 ms (37.19%) | Memory: 16.7 MB (10.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,10 +3,10 @@ class Solution:
         braces={"}":"{","]":"[",")":"("}
         stack=[]
         for i in range(len(s)):
-            if s[i] in braces  and stack  :
-                 if  stack[-1]==braces[s[i]]:
+            if s[i] in braces   :
+                 if stack and stack[-1]==braces[s[i]]:
                     stack.pop()
-                 else:break
+                 else:return False
             else:
                 stack.append(s[i])
         return True if len(stack)==0 else False
