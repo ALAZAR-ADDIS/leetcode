cc2c58cced7e8d427cf9da49f2e285b993f14551
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-10-22 03:45:10 -0700

    Time: 39 ms (98.33%) | Memory: 39.1 MB (99.06%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,24 +5,35 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        lg=0
-        dummy=ListNode(0,head)
-        r=dummy.next
-        while r:
-            r=r.next
-            lg+=1
-        r,l=dummy.next,dummy
-        lg=lg-k
+        # lg=0
+        # dummy=ListNode(0,head)
+        # r=dummy.next
+        # while r:
+        #     r=r.next
+        #     lg+=1
+        # r,l=dummy.next,dummy
+        # lg=lg-k
         
-        while lg>0:
-            r=r.next
-            lg-=1
-        while k>0:
-            l=l.next
-            k-=1
-        l.val,r.val=r.val,l.val
+        # while lg>0:
+        #     r=r.next
+        #     lg-=1
+        # while k>0:
+        #     l=l.next
+        #     k-=1
+        # l.val,r.val=r.val,l.val
 
-        return dummy.next
+        # return dummy.next
+
+        curr=head
+        for i in range(k-1):
+            curr=curr.next
+        l=curr
+        r=head
+        while curr.next:
+            r=r.next
+            curr=curr.next
+        l.val,r.val=r.val,l.val
+        return head
         
 
         
