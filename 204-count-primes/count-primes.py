aaefe56ae51b06e41f9b944ae1ec34c65e7c765a
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-11-07 13:04:01 -0800

    Time: 2023 ms (35.62%) | Memory: 70.2 MB (30.86%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -4,20 +4,18 @@ class Solution:
         ans=0
 
         for i in range(2,n):
+            if math.ceil(sqrt(n))<=i:
+                break
             if count[i]:
                 for j in range(i*i,n,i):
                     count[j]=0
-        for i in range(3,n+1):
+    
+        for i in range(2,n):
             if count[i]:
                 ans+=1
         return ans
     
-    def isPrime(num):
-        
-        for i in range(2,num//2+1):
-            if num%i==0:
-                return False
-        return True
+   
     
 
 
