e96ab2fd2eac4357fb7d3cebb1c5370b413fbc72
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-11-15 05:19:14 -0800

    Time: 202 ms (11.10%) | Memory: 16.9 MB (98.25%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,16 +1,30 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(heights)):
-            isChanged=False
-            for j in range(1,len(heights)-i):
+        # Bubble sort algorithm
+        # for i in range(len(heights)):
+        #     isChanged=False
+        #     for j in range(1,len(heights)-i):
                 
-                if heights[j-1]<heights[j]:
-                    heights[j-1],heights[j]= heights[j],heights[j-1]
-                    names[j-1],names[j]= names[j],names[j-1]
-                    isChanged=True
-            if not isChanged:
-                return names
+        #         if heights[j-1]<heights[j]:
+        #             heights[j-1],heights[j]= heights[j],heights[j-1]
+        #             names[j-1],names[j]= names[j],names[j-1]
+        #             isChanged=True
+        #     if not isChanged:
+        #         return names
+        # return names
+
+        # selection sort
+
+        for i in range(len(heights)):
+            maxIndex=i
+            for j in range(i+1,len(heights)):
+                if heights[maxIndex]<heights[j]:
+                    maxIndex=j
+            heights[maxIndex],heights[i]=heights[i],heights[maxIndex]
+            names[maxIndex],names[i]=names[i],names[maxIndex]
         return names
+                    
+
 
         
         
