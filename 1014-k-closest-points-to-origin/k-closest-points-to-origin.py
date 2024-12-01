7c9a46320cde4b1141f071184155e0179866f86f
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-12-01 09:03:17 -0800

    Time: 16 ms (99.73%) | Memory: 21.2 MB (86.85%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -2,7 +2,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def sorter(cord):
             x,y=cord
-            return x**2 + y**2
+            return (x**2 -0)+ (y**2-0)
         points.sort(key=sorter)
         return points[0:k]
     
