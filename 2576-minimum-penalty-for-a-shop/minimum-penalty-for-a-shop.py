92926700a0051de82876e2e38e2ee2830352f003
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-12-13 01:21:59 +0300

    Time: 179 ms (18.97%) | Memory: 25.4 MB (11.18%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,23 +1,27 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        
-        n = len(customers)
-        count_Y = count_N = 0
-        for i in range(len(customers)):
-            if customers[i] == 'Y':
-                count_Y += 1
+        Yes=[0]*(len(customers)+1)
+        No=[0]*(len(customers)+1)
 
-        min_ = count_Y
-        idx = 0
-        for i in range(n):  
-            if min_ > count_N + count_Y:
-                min_ = count_N + count_Y
-                idx = i
-            if customers[i] == 'Y':
-                count_Y -= 1
+        for i in range(1,len(Yes)):
+            if customers[i-1]=="Y":
+                Yes[i]=1
+                No[i]=0
             else:
-                count_N += 1 
+                Yes[i]=0
+                No[i]=1
+        for i in range(1,len(Yes)):
+            Yes[i]=Yes[i-1]+Yes[i]
+            No[i]=No[i-1]+No[i]
+        Min=0
+        val=float("inf")
 
-        if min_ > count_N + count_Y :
-            return n         
-        return idx
+        for i in range(len(Yes)):
+            if val>(No[i]+(Yes[-1]-Yes[i])):
+                val=No[i]+(Yes[-1]-Yes[i])
+                Min=i
+
+        return Min
+
+        
+       
