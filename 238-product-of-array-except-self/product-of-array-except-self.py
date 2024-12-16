cdc8979b6a4a407bddd6f5c4804de71c3463135e
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-12-16 09:58:49 +0300

    Time: 19 ms (86.32%) | Memory: 22.8 MB (47.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre=nums[::]
-        post=nums[::]
-        for i in range(1,len(nums)):
-            pre[i]*=pre[i-1]
-        for i  in range(len(nums)-2,-1,-1):
-            post[i]*=post[i+1]
-        print(post)
-        print(pre)
+        # pre=nums[::]
+        # post=nums[::]
+        # for i in range(1,len(nums)):
+        #     pre[i]*=pre[i-1]
+        # for i  in range(len(nums)-2,-1,-1):
+        #     post[i]*=post[i+1]
+     
     
-        for i in range(len(nums)):
-            pr=  1 if i==0 else pre[i-1]
-            pos= 1 if i==len(nums)-1  else post[i+1]
-            print(pr,pos)
+        # for i in range(len(nums)):
+        #     pr=  1 if i==0 else pre[i-1]
+        #     pos= 1 if i==len(nums)-1  else post[i+1]
+        
 
-            nums[i]=pr*pos
-        return nums
+        #     nums[i]=pr*pos
+        # return nums
+
+        #approch 2
+
+        res=[1]*len(nums)
+
+        pre=1
+        for i in range(len(nums)):
+            res[i]=pre
+            pre*=nums[i]
+        post=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]*=post
+            post*=nums[i]
+
+        return res
 
         
         
