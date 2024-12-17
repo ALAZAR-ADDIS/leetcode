edfb9f7f72c18efbd7a6b9c403adf66bf862df5c
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-12-18 01:09:40 +0300

    Time: 1 ms (62.52%) | Memory: 17.9 MB (9.62%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,19 +1,47 @@
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+        
+#         wordFreq={}
+#         total=0
+#         for i in s:
+#             wordFreq[i]=wordFreq.get(i,0)+1
+#             if wordFreq[i]%2==0:
+#                 total+=2
+        
+
+#         for i in wordFreq.values():
+#             if i%2!=0:
+#                total+=1
+#                break
+#         return total 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         
         wordFreq={}
-        total=0
         for i in s:
             wordFreq[i]=wordFreq.get(i,0)+1
-            if wordFreq[i]%2==0:
-                total+=2
-        
+        total=0
+        maxOdd=0
 
+        print(wordFreq)
         for i in wordFreq.values():
-            if i%2!=0:
-               total+=1
-               break
-        return total 
+            print(i)
+            if i%2:
+               
+                if maxOdd<=i:
+                    total+=maxOdd-1 if maxOdd else 0
+                    maxOdd=i
+                   
+                else:
+                    total+=i-1
+                continue
+        
+            total+=i
+            print(total)
+        print(maxOdd)
+        print(total)
+        return total + maxOdd
+            
             
                 
 
