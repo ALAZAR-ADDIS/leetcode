0b9a0390926471419c1d2532a2c1a0eb538d217f
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2024-12-24 04:50:31 +0300

    Time: 3 ms (44.95%) | Memory: 17.8 MB (5.00%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -5,12 +5,11 @@ class Solution:
         r=0
         while r<len(popped):
             while (l<len(pushed))  and (not stack or stack[-1]!=popped[r]):
-                
                 stack.append(pushed[l])
                 l+=1
-                if stack[-1]==popped[r]:break
+               
                 
-            print(stack[-1],popped[r])
+           
             if stack[-1]==popped[r]:
                 stack.pop()
             else:
