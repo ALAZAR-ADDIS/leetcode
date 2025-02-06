5f74eb9939aebd817589c8af2cd4de9ddb348d07
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-06 20:52:28 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.62%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -1,16 +1,14 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        for i in image:
-            l,r=0,len(i)-1
-            while l<=r:
-                i[r],i[l]=i[l],i[r]
-                if l!=r:
-                    i[l]=0 if i[l]==1 else 1
-                    i[r]=0 if i[r]==1 else 1
+        for  i in range(len(image)):
+            image[i] = image[i][::-1]
+        
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if image[i][j]:
+                    image[i][j]=0
                 else:
-                    i[l]=0 if i[l]==1 else 1
-            
-                l+=1
-                r-=1
+                     image[i][j]=1
         return image
+
         
