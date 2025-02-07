64aa60942945e185177964539363d61ee00a430c
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-07 14:31:27 +0300

    Time: 27 ms (5.19%) | Memory: 17.7 MB (61.83%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -7,24 +7,27 @@ class Solution:
             for i in range(len(board)):
 
                 num_count[board[i][j]] += 1 if board[i][j] != "." else 0 
+
                 if num_count[board[i][j]] >1:                                      
-                            return False              
+                            return False            
                                 
                 if  i%3 == 0 and j %3 ==0:
 
-                    in_count = Counter() 
-                                                 
+                    in_count = Counter()                  
                    
                     for row in range(i,i+3):
                         for col in range(j, j+3):
-                                    
-                            in_count[board[row][col]] += 1 if board[row][col] != "." else 0
-                                            
-                                                      
+                          
+                            
+                            
+                            in_count[ board[row][col]] += 1 if board[row][col] != "." else 0
+
+                       
+                            
+                           
                             if in_count[board[row][col]] > 1:                      
                                 
                                     return False
-                print(num_count)
         
                     
 
