d9474fadcfd985c738c96188da9eb638b56ec436
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-07 09:30:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.34%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,18 +3,69 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for i  in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        # for i  in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
 
-                if i <= j:
-                    matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+        #         if i <= j:
+        #             matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
     
         
-        for i in range(len(matrix)):
-            matrix[i] = matrix[i][::-1]
+        # for i in range(len(matrix)):
+        #     matrix[i] = matrix[i][::-1]
             
         
       
         
 
-        return matrix
+        # # return matrix
+        
+        # #approch 2
+
+
+        l= 0
+        r = len(matrix)-1
+
+        while l < r:
+            
+            matrix[l], matrix[r] =  matrix[r], matrix[l]
+
+            l += 1
+            r -= 1
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # print(matrix)
+
+        
+        for i  in range(len(matrix)):
+            for j in range(len(matrix[0])):
+
+                if i <= j:
+                    matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        
+        # return  matrix
+             
+        
+        
+    
+        
+
