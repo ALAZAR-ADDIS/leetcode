0b89b20d652605e0740d80e42e5020b63ad90cd8
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-07 14:49:45 +0300

    Time: 23 ms (5.19%) | Memory: 17.9 MB (21.95%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -9,15 +9,32 @@ class Solution:
                 num_count[board[i][j]] += 1 if board[i][j] != "." else 0 
 
                 if num_count[board[i][j]] >1:                                      
-                            return False            
-                                
+                            return False  
+                           
+              
+             
+
+        for i in range(len(board)):
+            num_count=Counter()
+
+            for j in range(len(board[0])):
+
+                num_count[board[i][j]] += 1 if  board[i][j] != "." else 0
+
+                if num_count[board[i][j]] > 1:
+                     return False 
+        
+        start_index=[[0,0],[0,3],[0,6], [3,0],[3,3],[3,6], [6,0],[6,3],[6,6]]
+        
+
+        for i, j in start_index:
+            
                 if  i%3 == 0 and j %3 ==0:
 
                     in_count = Counter()                  
                    
                     for row in range(i,i+3):
-                        for col in range(j, j+3):
-                          
+                        for col in range(j, j+3):                       
                             
                             
                             in_count[ board[row][col]] += 1 if board[row][col] != "." else 0
@@ -28,22 +45,6 @@ class Solution:
                             if in_count[board[row][col]] > 1:                      
                                 
                                     return False
-        
-                    
-
-                    
-
-                        
-
-        for i in range(len(board)):
-            num_count=Counter()
-
-            for j in range(len(board[0])):
-
-                num_count[board[i][j]] += 1 if  board[i][j] != "." else 0
-
-                if num_count[board[i][j]] > 1:
-                     return False            
         
         return True
                         
