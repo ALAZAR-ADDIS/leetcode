6d4b0226b5520e7dcdf74ab84a333478d0c21c2d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-10 14:56:20 +0300

    Time: 677 ms (5.03%) | Memory: 18.1 MB (65.23%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,49 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # Bubble sort algorithm
-        # for i in range(len(heights)):
-        #     isChanged=False
-        #     for j in range(1,len(heights)-i):
-                
-        #         if heights[j-1]<heights[j]:
-        #             heights[j-1],heights[j]= heights[j],heights[j-1]
-        #             names[j-1],names[j]= names[j],names[j-1]
-        #             isChanged=True
-        #     if not isChanged:
-        #         return names
-        # return names
 
-        # selection sort
-
-        # for i in range(len(heights)):
-        #     maxIndex=i
-        #     for j in range(i+1,len(heights)):
-        #         if heights[maxIndex]<heights[j]:
-        #             maxIndex=j
-        #     heights[maxIndex],heights[i]=heights[i],heights[maxIndex]
-        #     names[maxIndex],names[i]=names[i],names[maxIndex]
-        # return names
-
-        #inseertion sort
-        
-        for i  in range(1,len(heights)):
-            j=i-1
-            maxNum=heights[i]
-            maxName=names[i]
-            while j>=0 and heights[j]<maxNum:
-                heights[j+1],heights[j]=heights[j],heights[j+1]
-                names[j+1],names[j]=names[j],names[j+1]
-                j-=1
-            heights[j+1]=maxNum
-            names[j+1]=maxName
+        for i in range(len(heights)-1):
+            for j in range(len(heights)-i-1):
+                if heights[j] < heights[j+1]:
+                    heights[j] , heights[j+1] = heights[j+1], heights[j]
+                    names[j] , names[j+1] = names[j+1], names[j]
         print(heights)
         return names
-                
-
-
-
-                    
-
-
-        
         
