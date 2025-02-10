66f14f84b72fd2d8f6dec85e9f4fbb348613e3fe
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-10 16:05:30 +0300

    Time: 153 ms (12.55%) | Memory: 19 MB (7.95%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -19,23 +19,45 @@ class Solution:
         # return names
 
 
-        for i in range(1,len(heights)):
-            j= i-1
-            temp_h = heights[i]
-            temp_n = names[i]
+        # for i in range(1,len(heights)):
+        #     j= i-1
+        #     temp_h = heights[i]
+        #     temp_n = names[i]
             
 
-            while j>=0 and heights[j] < temp_h:
+        #     while j>=0 and heights[j] < temp_h:
 
-                heights[j+1] = heights[j]  
-                names[j+1] = names[j]
+        #         heights[j+1] = heights[j]  
+        #         names[j+1] = names[j]
+        #         j-=1
+        #     names[j+1] = temp_n
+        #     heights[j+1] = temp_h
+            
+            
+        # return names
+        
+        dict_pair = {}
+        for i in range(len(names)):
+            dict_pair[heights[i]] = names[i]
+        
+        max_val = max(heights)
+        counter = [0]*(max_val + 1)
+
+        for i in  heights:
+            counter[i] += 1
+        
+        ans = []
+
+        for  i in range(len(counter)-1,-1,-1):
+            j = counter[i]
+            while  j>0:
+                ans.append(dict_pair[i])
                 j-=1
-            names[j+1] = temp_n
-            heights[j+1] = temp_h
-            
-            
-        return names
-            
+        return ans
+        
+
+
+
 
 
 
