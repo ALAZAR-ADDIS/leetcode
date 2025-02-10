ff7fa4f25f9ca4d96791019588e2cdd6eccd160d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-10 15:39:32 +0300

    Time: 460 ms (7.41%) | Memory: 18.4 MB (7.95%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -8,14 +8,32 @@ class Solution:
                     # names[j] , names[j+1] = names[j+1], names[j]
        
         # return names
-        for i in range(len(heights)):
-            max_index= i
-            for j in range(i+1,len(heights)):
-                if heights[max_index] < heights[j]:
-                    max_index =j
+        # for i in range(len(heights)):
+        #     max_index= i
+        #     for j in range(i+1,len(heights)):
+        #         if heights[max_index] < heights[j]:
+        #             max_index =j
             
-            heights[i] , heights[max_index] = heights[max_index],  heights[i]
-            names[i] , names[max_index] = names[max_index], names[i]
+        #     heights[i] , heights[max_index] = heights[max_index],  heights[i]
+        #     names[i] , names[max_index] = names[max_index], names[i]
+        # return names
+
+
+        for i in range(1,len(heights)):
+            j= i-1
+            temp_h = heights[i]
+            temp_n = names[i]
+            
+
+            while j>=0 and heights[j] < temp_h:
+
+                heights[j] , heights[j+1] = heights[j+1], heights[j]  
+                names[j] , names[j+1] = names[j+1], names[j]
+                j-=1
+            names[j+1] = temp_n
+            heights[j+1] = temp_h
+            
+            
         return names
             
 
