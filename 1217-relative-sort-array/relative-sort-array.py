69cb4d35202e97818f4eefc64684def96d78d387
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-10 18:31:41 +0300

    Time: 4 ms (32.77%) | Memory: 18 MB (36.81%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,27 +1,20 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        list1=[0]*(max(arr1)+1)
-        ans=[]
-        
-        for i in arr1:
-            list1[i]+=1
-        index=0
-        for i in arr2:
-            
-            j=list1[i]
-            while j>0:
-                arr1[index]=i
-                list1[i]-=1
-                index+=1
-                j-=1
-        
+        max_num = max(arr1)
+        count_num = [0] * (max_num + 1)
+        ans = []
 
-        print(list1)
-        for i in range(len(list1)):            
-            j=list1[i]                
-            while j>0:
-                arr1[index]=i
-                index+=1
-                j-=1
-        return arr1
+        for i in arr1:
+            count_num[i] += 1
+        
+        for val in arr2:
+            for i in range(count_num[val]):
+                ans.append(val)
+                count_num[val]-=1
+        
+        for index, val in enumerate(count_num):
+            for i in range(val):
+                ans.append(index)
+        return ans
+
         
