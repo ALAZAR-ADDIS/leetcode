4707430947c522d936315039743fc1a4c8e5c9aa
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-11 10:28:21 +0300

    Time: 3 ms (69.56%) | Memory: 18.1 MB (16.11%) - LeetSync

--- 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.py
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.py
@@ -1,24 +1,44 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
+        # xy = 0
+        # xz =0
+        # yz = 0
+
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j]:
+        #             xy += 1
+        #     xz += max(grid[i])
+       
+        
+        # for j in range(len(grid[0])):
+        #     temp = - float("inf")
+        #     for i in range(len(grid)):                                 
+        #         temp = max(temp, grid[i][j])
+       
+        #     yz += temp
+       
+
+        # return xy + xz + yz
+
         xy = 0
         xz =0
         yz = 0
 
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]:
-                    xy += 1
             xz += max(grid[i])
        
         
         for j in range(len(grid[0])):
             temp = - float("inf")
             for i in range(len(grid)):
+                if grid[i][j]:
+                    xy += 1                                 
                 temp = max(temp, grid[i][j])
        
             yz += temp
        
 
-        return xy + xz + yz 
+        return xy + xz + yz  
     
         
