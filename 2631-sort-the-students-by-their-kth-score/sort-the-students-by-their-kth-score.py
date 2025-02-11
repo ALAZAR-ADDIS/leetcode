4f782ca281437ba671bd794231c16de1e745905d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-11 13:58:29 +0300

    Time: 6 ms (23.13%) | Memory: 22.8 MB (21.57%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -4,7 +4,7 @@ class Solution:
         # return score[::-1]
 
         self.divide(score,0,len(score)-1,k)
-        return score[::-1]
+        return score
 
         
 
@@ -16,28 +16,36 @@ class Solution:
             self.merge(arr,l,mid,r,k)
 
 
-    def merge(self,nums,left,mid,right,k):
+    def merge(self,arr,left,mid,right,k):
 
-        
+
+
         temp = []
-        l = left
+        l = left 
         r = mid + 1
-        while l <= mid or r <= right:
 
-            if  r > right or (l <= mid and self.compare(nums[l] , nums[r],k)):
-                temp.append(nums[l])
-                l += 1
-            
+      
+        while l <= mid or r <= right:     
+        
+            if r > right or (mid >= l and self.compare(arr[l],arr[r],k)):
+                temp.append(arr[l])
+                l+=1
             else:
-                temp.append(nums[r])
-                r += 1
-        for i  in range(left, right +1):
-            nums[i] = temp[i - left]
+
+                temp.append(arr[r])
+                r+=1
+          
+        
+        
+        
+        for i in range(left,right + 1):
+                arr[i] = temp[i-left]
+       
 
 
 
 
     def compare(self,row1,row2,k):
    
-        return row1[k] <= row2[k]
+        return row1[k] >= row2[k]
         
