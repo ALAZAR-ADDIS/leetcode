e006ff1817c39072319a9050c7a62c9e3a9aa574
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-11 11:26:09 +0300

    Time: 15 ms (5.08%) | Memory: 17.7 MB (54.87%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,36 +1,50 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        nums = list(map(str,nums))
-        self.divide(nums,0,len(nums)-1)
+        nums = list(map(str, nums))
 
+        for i in range(len(nums)-1):
+            for j in range(len(nums)-i -1):
+                if not self.compare(nums[j],nums[j+1]):
+                    nums[j],nums[j+1] = nums[j+1], nums[j]
+        
         return str(int("".join(nums)))
+
+
+        #using merge sort
+    #     nums = list(map(str,nums))
+    #     self.divide(nums,0,len(nums)-1)
+
+    #     return str(int("".join(nums)))
     
 
-    def divide(self,nums,left,right):
-        if left < right :
-            mid = (left + right)//2
-            self.divide(nums,left,mid)
-            self.divide(nums,mid + 1, right)
-            self.merge(nums,left,mid,right)
+    # def divide(self,nums,left,right):
+    #     if left < right :
+    #         mid = (left + right)//2
+    #         self.divide(nums,left,mid)
+    #         self.divide(nums,mid + 1, right)
+    #         self.merge(nums,left,mid,right)
     
-    def merge(self,nums,left,mid,right):
-        temp = []
-        l = left
-        r = mid + 1
-        while l <= mid or r <= right:
+    # def merge(self,nums,left,mid,right):
+    #     temp = []
+    #     l = left
+    #     r = mid + 1
+    #     while l <= mid or r <= right:
 
-            if  r > right or (l <= mid and self.compare(nums[l] , nums[r])):
-                temp.append(nums[l])
-                l += 1
+    #         if  r > right or (l <= mid and self.compare(nums[l] , nums[r])):
+    #             temp.append(nums[l])
+    #             l += 1
             
-            else:
-                temp.append(nums[r])
-                r += 1
-        for i  in range(left, right +1):
-            nums[i] = temp[i - left]
+    #         else:
+    #             temp.append(nums[r])
+    #             r += 1
+    #     for i  in range(left, right +1):
+    #         nums[i] = temp[i - left]
+
     
     def compare(self,num1,num2):
         return num1 + num2 >=  str(num2) + str(num1)
+
+    
 
 
         
