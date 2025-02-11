bc1cd28d75193fe6a0c9a5fb21c8f3b37343f8a0
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-11 16:50:20 +0300

    Time: 15 ms (7.11%) | Memory: 18.2 MB (13.44%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,11 +1,19 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         
-        ans = [0]*len(nums)
+        # ans = [0]*len(nums)
 
+        # for i in range(len(nums)):
+
+        #     ans[i]=nums[nums[i]]
+        
+        # return ans
+        
         for i in range(len(nums)):
-            
-            ans[i]=nums[nums[i]]
+            val = nums[i]  % len(nums)
+            nums[i] = (nums[val] % len(nums)) * len(nums) + val
         
-        return ans
+        for i in range(len(nums)):
+            nums[i] //= len(nums)
         
+        return nums
