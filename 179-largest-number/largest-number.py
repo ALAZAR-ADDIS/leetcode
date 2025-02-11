cc25a2fb01e4a7be4990cbd678fa28d4c9d1c64a
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-11 09:27:49 +0300

    Time: 4 ms (25.83%) | Memory: 17.9 MB (14.60%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,13 +1,36 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        for i,n in enumerate(nums):
-            nums[i]=str(n)
-        def compare(n1,n2):
-            if n1+n2>n2+n1:
-                return -1
-            else:
-                return 1
-        nums=sorted(nums,key=cmp_to_key(compare))
+        nums = list(map(str,nums))
+        self.divide(nums,0,len(nums)-1)
+
         return str(int("".join(nums)))
-        
+    
+
+    def divide(self,nums,left,right):
+        if left < right :
+            mid = (left + right)//2
+            self.divide(nums,left,mid)
+            self.divide(nums,mid + 1, right)
+            self.merge(nums,left,mid,right)
+    
+    def merge(self,nums,left,mid,right):
+        temp = []
+        l = left
+        r = mid + 1
+        while l <= mid or r <= right:
+
+            if  r > right or (l <= mid and self.compare(nums[l] , nums[r])):
+                temp.append(nums[l])
+                l += 1
+            
+            else:
+                temp.append(nums[r])
+                r += 1
+        for i  in range(left, right +1):
+            nums[i] = temp[i - left]
+    
+    def compare(self,num1,num2):
+        return num1 + num2 >=  str(num2) + str(num1)
+
+
         
