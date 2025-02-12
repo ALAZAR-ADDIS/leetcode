544169a852f8a59256ee12eaf529ef119692376d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-12 16:00:50 +0300

    Time: 4 ms (63.41%) | Memory: 17.8 MB (33.39%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,17 +1,22 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        dict={}
-        for i,n in enumerate(s):
-            dict[n]=i
-        ans =[]
-        end=0
-        size=0
-        for i,n in enumerate(s):
-            size+=1
-            end=max(end,dict[n])
-            if end==i:
-                ans.append(size)
-                size=0
-        return ans
+        curr_char = set()
+        last_index = {}
+        ans = []
+        l=0
+        for index,char in enumerate(s):
+            last_index[char] = index
+        
+        for index,char  in enumerate(s):
+            curr_char.add(char)
 
+            if index == last_index[char]:
+                curr_char.remove(char)
+            
+            if not len(curr_char) :
+                ans.append(index - l +1)
+                l = index + 1
+        return ans
+                
+        
         
