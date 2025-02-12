b1774ba0b98715320d328f1f0521ed4953ef1bac
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-12 11:35:03 +0300

    Time: 79 ms (57.27%) | Memory: 29 MB (78.94%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -1,25 +1,12 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        list1=[0]*(max(costs)+1)
-        for  i in costs:
-            list1[i]+=1
-        total=0
-        count=0
-        for i in range(len(list1)):
-            j=list1[i]
-            while j>0:
-                if total+i>coins:
-                    return count
-                elif total+i==coins:
-                    return count+1
-               
-                
-                
-                total+=i
-                j-=1
-                count+=1
-        return count
-            
+        costs.sort()
+        i =0
 
-
+        while i< len(costs):
+            if coins - costs[i] < 0 :
+                return i 
+            coins -= costs[i]
+            i += 1
+        return i
         
