51f14cf8e64dc795eb82416bded9fe9ac6a714aa
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-12 15:21:48 +0300

    Time: 57 ms (71.78%) | Memory: 17.9 MB (31.79%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,16 +1,17 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        r=int(sqrt(c))+1
-        l=0
-        while l<=r:
-            total=l**2+r**2
-            if total==c:
-                return True
-            elif total>c:
-                r-=1
-            else:
-                l+=1
-        return False
-            
+        val = int(sqrt(c))
+        l = 0
+        r= val + 1
 
+        while l <= r:
+            sqr = l**2 + r**2
+            if sqr == c:
+                return True
+            elif sqr > c:
+                r -= 1
+            else:
+                l += 1
+                
+        return False
         
