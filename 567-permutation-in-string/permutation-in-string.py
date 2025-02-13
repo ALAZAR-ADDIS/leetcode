e14914be23ecb8984699795c2d24af4b1b96b861
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-13 21:45:04 +0300

    Time: 23 ms (54.12%) | Memory: 17.8 MB (59.22%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,22 +1,29 @@
 class Solution:
-     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Dict={}
-        s2Dict={}
-        l=0
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        dict_s1 = defaultdict(int)
+        dict_s2 = defaultdict(int)
+        l = 0
+
         for i in range(len(s1)):
-            s1Dict[s1[i]]=s1Dict.get(s1[i],0)+1
-        
+            dict_s1[s1[i]] += 1 
+
         for r in range(len(s2)):
-            s2Dict[s2[r]]=s2Dict.get(s2[r],0)+1
-            window=r-l+1
-            if window>len(s1):
-                if s2Dict[s2[l]]==1:
-                    s2Dict.pop(s2[l])
-                else :
-                    s2Dict[s2[l]]-=1
-                l+=1
-            
-            if s2Dict==s1Dict:
+            dict_s2[s2[r]] += 1
+
+            while r - l + 1 > len(s1):
+
+                dict_s2[s2[l]]  -= 1
+
+                if dict_s2[s2[l]] == 0:
+
+                    dict_s2.pop(s2[l])
+                l += 1
+   
+            if dict_s1 == dict_s2:
                 return True
         return False
-                
+
+        
+        
+        
+        
