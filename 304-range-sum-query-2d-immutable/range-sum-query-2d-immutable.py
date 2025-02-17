bd363e240ef723358ceb3fd8cb61819e8ce7ef50
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-17 16:40:30 +0300

    Time: 118 ms (52.83%) | Memory: 30.9 MB (64.25%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,20 +1,25 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.nums= [[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
+        row = len(matrix)
+        col = len(matrix[0])
         
-        for i in range(len(matrix)):
-            prefix=0
-            for j in range(len(matrix[0])): 
-                prefix+=matrix[i][j]    
-                above=self.nums[i][j+1]          
-                self.nums[i+1][j+1]=prefix+above
-        print(self.nums)
-                
+        self.mat = [[0]*(col + 1) for _ in range(row + 1)]
+
+        for i in range(row):
+            for j in range(col):
+                self.mat[i + 1][j + 1] = matrix[i][j] + self.mat[i][j + 1] + self.mat[i + 1][j ] - self.mat[i][j]
+     
+        
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.nums[row2+1][col2+1]-self.nums[row2+1][col1]-self.nums[row1][col2+1]+self.nums[row1][col1]
+
+        return self.mat[row2 + 1][col2 + 1] + self.mat[row1  ][col1  ] - self.mat[row1][col2 + 1] - self.mat[row2 + 1][col1]
+
+      
+
+        
         
 
 
