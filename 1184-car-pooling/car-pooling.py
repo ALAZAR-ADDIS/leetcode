4bbb9d3684a527ed4ec3842791ea636136d6df0f
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-20 11:48:37 +0300

    Time: 1 ms (78.09%) | Memory: 18.1 MB (94.41%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,13 +1,19 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        check=[0]*(1000+1)
-        for i in trips:
-            check[i[1]]+=i[0]
-            check[i[2]]-=i[0]
-        for i  in range(1,len(check)):
-            check[i]=check[i]+check[i-1]
-        for i in range(len(check)):
-            if check[i]>capacity:
-                return    False
+        nums =  [0]* 1002
+        for pp,star,end in trips:
+            nums[star] += pp
+            nums[end ] += -pp
+        
+        for i in range(1,len(nums)):
+            nums[i] += nums[i- 1]
+
+            if nums[i] > capacity:
+                return False
+
+        for i in range(len(nums)):          
+
+                if nums[i] > capacity:
+                    return False
         return True
         
