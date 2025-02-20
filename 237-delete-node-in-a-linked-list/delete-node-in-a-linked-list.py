a7e8c84d61133fb0ef53db75483f31cd29adc59d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-20 18:57:06 +0300

    Time: 48 ms (14.78%) | Memory: 18.1 MB (28.23%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -5,24 +5,19 @@
 #         self.next = None
 
 class Solution:
-    def deleteNode(self,node):
+    def deleteNode(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        # l=node
-        # r=node.next
-        # while r.next:
-        #     l.val=r.val
-        #     r=r.next
-        #     l=l.next
-        # l.val=r.val
-        # l.next=None
-        l=node
-        r=node.next
-        l.val=r.val
-        l.next=r.next
+
+        temp = node.next
+        prev= node
         
-        
-        
+        while  temp.next:
+            prev.val = temp.val
+            temp = temp.next
+            prev = prev.next
+        prev.val = temp.val
+        prev.next = None
         
