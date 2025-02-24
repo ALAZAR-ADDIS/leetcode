65d5bd5926003e213ae5d9c1fa00033b3ad294cf
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-24 17:54:33 +0300

    Time: 63 ms (19.71%) | Memory: 38.2 MB (35.13%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -2,12 +2,13 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
 
         dict_count = defaultdict(int)
+        dict_count[0] = -1
         summ = 0
 
         for i in range(len(nums)):
             summ += nums[i]
 
-            if  (summ % k == 0 and i >=1) or (summ % k in dict_count) and (i - dict_count[summ % k] ) >=2:
+            if  (summ % k in dict_count) and (i - dict_count[summ % k] ) >=2:
                 return True
                 
             if summ % k not in dict_count:
