3e8a6672af2ccc21f33141634c672ef5d740e995
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-26 08:55:05 +0300

    Time: 115 ms (26.84%) | Memory: 23.5 MB (50.61%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,17 +1,36 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        # dict_count = defaultdict(int)
+        # dict_count[0] = -1
+        # summ = 0
+        # ans = 0
+        # for i in range(len(nums)):
+        #     summ += 1 if nums[i] else -1
+            
+        #     if summ in dict_count:
+        #         ans = max(ans, i - dict_count[summ])
+        #     else:
+        #         dict_count[summ] = i      
+            
+
+        # return ans
+
+        ones = 0
+        zeros = 0
         dict_count = defaultdict(int)
         dict_count[0] = -1
-        summ = 0
         ans = 0
-        for i in range(len(nums)):
 
-            summ += 1 if nums[i] else -1
-            
-            if summ in dict_count:
-                ans = max(ans, i - dict_count[summ])
+        for i in range(len(nums)):
+            ones += 1 if nums[i] else 0
+            zeros += 0 if nums[i] else 1
+            diff  = ones - zeros
+
+            if  diff in dict_count:
+                ans = max(ans, i - dict_count[diff])
             else:
-                dict_count[summ] = i
+                dict_count[diff] = i
+
 
         return ans
 
