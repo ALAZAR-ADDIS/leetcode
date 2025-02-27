cc762cbb4a6b31eb66d06bf17928239bef73df00
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-02-27 10:10:31 +0300

    Time: 2 ms (82.51%) | Memory: 19.3 MB (7.10%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.py
@@ -1,22 +1,27 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        ans = float("inf")
-        pref = [0] * (len(nums) + 1)
-        summ = sum(nums)
+        # ans = float("inf")
+        # pref = [0] * (len(nums) + 1)
+        # summ = sum(nums)
+        # nums.sort()
+        
+        
+
+        # for i in range(1,len(pref)):
+        #     pref[i] += pref[i - 1] + nums[i - 1]
+        
+        # for i in range(1,len(pref)):
+        #     pre = nums[i-1] * (i-1) - pref[i - 1]
+        #     post = summ - pref[i - 1] - (nums[i - 1] *(len(pref) - i))
+
+        #     ans = min(ans , pre + post)
+
+        # return ans
         nums.sort()
-        
-        
+        mid = len(nums)//2
+        target = nums[mid]
 
-        for i in range(1,len(pref)):
-            pref[i] += pref[i - 1] + nums[i - 1]
         
-        for i in range(1,len(pref)):
-            pre = nums[i-1] * (i-1) - pref[i - 1]
-            post = summ - pref[i - 1] - (nums[i - 1] *(len(pref) - i))
-
-            ans = min(ans , pre + post)
-
-        return ans
-        
+        return sum([abs(target - n) for n in nums])
 
         
