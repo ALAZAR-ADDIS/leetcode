4e0bf12ad92300dafaab4e381243fcc8ceabdde0
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-04 19:07:16 +0300

    Time: 15 ms (11.90%) | Memory: 21.8 MB (95.21%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,27 +1,46 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
+        changes = [10,5]
         
         curr = defaultdict(int)
 
         for i in range(len(bills)):
             change = bills[i] - 5
+            j = 0
 
-            if change == 15:
-                if curr[10] >= 1 and curr[5] >= 1:
-                    curr[10] -= 1
-                    curr[5] -=1
-                elif curr[5] >=3 :
-                    curr[5] -= 3
-                else:
-                    return False
+            while change and j < len(changes) :
 
-            elif change >= 5:
-                if curr[5] >=1 :
-                    curr[5] -= 1
-                else:
-                    return False 
-            curr[bills[i]] += 1
-        return True
+                if change < changes[j] or not  curr[changes[j]]:
+                    j += 1
+
+                else:                    
+                        change -= changes[j]
+                        curr[changes[j]] -= 1
+                  
+            if change:
+                return False
+            curr[bills[i]] +=1 
+
+        return  True
+
+
+
+            # if change == 15:
+            #     if curr[10] >= 1 and curr[5] >= 1:
+            #         curr[10] -= 1
+            #         curr[5] -=1
+            #     elif curr[5] >=3 :
+            #         curr[5] -= 3
+            #     else:
+            #         return False
+
+            # elif change >= 5:
+            #     if curr[5] >=1 :
+            #         curr[5] -= 1
+            #     else:
+            #         return False 
+            # curr[bills[i]] += 1
+        # return True
                 
                 
                        
