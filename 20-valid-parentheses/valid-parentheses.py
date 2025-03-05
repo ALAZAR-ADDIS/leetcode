017fb0a6b5ea09d1318e9c945f9d937510c31015
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-05 17:42:05 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,22 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #check if the element in the  list is close or open bracket
-        #if open move  add to the stack
-        #else check i the stak has and element that can be pop end  is suitable closing bracket if so pop else return false
-        stack=[]
-        closingBrackets={"}",")","]"}
-        Dict={"}":"{","]":"[",")":"("}
+
+        closing = {"]", "}", ")"}
+
+        map = {"]":"[",")":"(", "}":"{"}
+        stack = []
 
         for i in range(len(s)):
-            if    s[i] in closingBrackets:
-                if stack and  stack[-1]==Dict[s[i]]:
+
+            if s[i] in closing:
+                if stack and stack[-1] == map[s[i]]:
                     stack.pop()
                     continue
                 else:
                     return False
 
             stack.append(s[i])
+        
         return False if len(stack) else True
-            
-
-       
