24e8c94916a058b037a0e1b81dcabe84024aa95d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-08 06:24:12 +0300

    Time: 1 ms (50.91%) | Memory: 18.1 MB (31.93%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,12 +1,23 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
 
-        que = deque()
+        # que = deque()
 
-        for num in sorted(deck, reverse = True):
+        # for num in sorted(deck, reverse = True):
+        #     if que:
+        #         que.appendleft(que.pop())
+        #     que.appendleft(num)
+        
+        # return list(que)
+
+
+        que = deque(range(len(deck)))
+        ans = [0] * len(deck)
+        deck.sort()
+
+        for n in deck:
+            ans[que.popleft()] = n
+
             if que:
-                que.appendleft(que.pop())
-            que.appendleft(num)
-        
-        return list(que)
-        
+                que.append(que.popleft()) 
+        return ans
