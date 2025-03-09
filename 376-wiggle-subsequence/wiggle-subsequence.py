ee7283d2ff29fbea3d499678025bbfaa9ca2b1fd
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-09 15:57:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.61%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.py
+++ 376-wiggle-subsequence/wiggle-subsequence.py
@@ -3,39 +3,82 @@ class Solution:
         if len(nums) == 1:
             return 1      
 
-        i = 1
-        val = nums[1] - nums[0]
-        ans = 1
+        prev = -(nums[1] - nums[0])
+        ans = 1   
 
-        if val > 0 or val < 0:
-            ans  += 1
+
+        for i in range(1,len(nums)):
+             
+            val = nums[i] - nums[i - 1]
+            if (prev > 0 and val < 0) or (prev < 0 and val > 0):
+                ans += 1
+            elif prev == 0 and val:
+                ans += 1
+            
+            if val:
+                prev = val
+
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
      
 
-        while i < len(nums):
+        # while i < len(nums):
            
            
-            if val > 0:
+        #     if val > 0:
                 
-                while i < len(nums) and val >= 0 :
-                    if nums[i] - nums[i - 1] < 0:
-                        ans += 1
-                    val = nums[i] - nums[i - 1]
-                    i += 1
-            elif val < 0:
+        #         while i < len(nums) and val >= 0 :
+        #             if nums[i] - nums[i - 1] < 0:
+        #                 ans += 1
+        #             val = nums[i] - nums[i - 1]
+        #             i += 1
+        #     elif val < 0:
 
-                while i < len(nums) and val <= 0 :
-                    if nums[i] - nums[i - 1] > 0:
-                        ans += 1
-                    val = nums[i] - nums[i - 1]
-                    i += 1      
+        #         while i < len(nums) and val <= 0 :
+        #             if nums[i] - nums[i - 1] > 0:
+        #                 ans += 1
+        #             val = nums[i] - nums[i - 1]
+        #             i += 1      
 
-            else :
+        #     else :
                 
-                while i < len(nums) and val == 0 :
+        #         while i < len(nums) and val == 0 :
 
-                    if nums[i] - nums[i - 1] > 0 or nums[i] - nums[i - 1] < 0  :
-                        ans += 1
-                    val = nums[i] - nums[i - 1]
-                    i += 1
-        return ans
-        
+        #             if nums[i] - nums[i - 1] > 0 or nums[i] - nums[i - 1] < 0  :
+        #                 ans += 1
+        #             val = nums[i] - nums[i - 1]
+        #             i += 1
+        # return ans
+
+
+       
