2e6d93d266dd90695a38cee60f872d96031d2dfd
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-09 06:39:45 +0300

    Time: 8 ms (52.78%) | Memory: 18.3 MB (35.94%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,28 +1,16 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # flowerdPlots=set()   
+        pot = [0]*(len(flowerbed) + 2)
 
-
-        # for r in range(len(flowerbed)):
-        #     if flowerbed[r]==0 and r not in flowerdPlots :
-        #         if len(flowerbed)>1 and  r==0 and flowerbed[r+1]==0:
-        #             flowerdPlots.add(r)
-        #             n-=1
-        #         elif  r-1 not in flowerdPlots and r==len(flowerbed)-1 and flowerbed[r-1]==0:
-        #             flowerdPlots.add(r)
-        #             n-=1
-        #         elif  r-1 not in flowerdPlots and flowerbed[r-1]==0 and flowerbed[r+1]==0:
-        #             flowerdPlots.add(r)
-        #             n-=1
-        # return  True  if n<=0 else False
-
-        FlowerB=[0]+flowerbed+[0]
-
-        for i in range(1,len(FlowerB)-1):
-            if FlowerB[i]==0 and FlowerB[i-1]==0 and FlowerB[i+1]==0:
-                n-=1
-                FlowerB[i]=1
-        return  True  if n<=0 else False
+        for i in  range(len(flowerbed)):
+            pot[i + 1] = flowerbed[i]
         
+
+        for i in range(1,len(pot)-1):
+            if pot[i - 1] == pot[i] == pot[i + 1] == 0:
+                pot[i] = 1
+                n -= 1
+        return n <= 0
+
         
         
