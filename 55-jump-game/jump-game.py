b59a95cb144f9145ad65e1c17207a1fc685c89e6
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-09 07:33:42 +0300

    Time: 21 ms (59.85%) | Memory: 18.3 MB (99.63%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,26 +1,31 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        jamp_zero = True
-        index = 0
+        # jamp_zero = True
+        # index = 0
 
-        if len(nums) == 1:
-            return True
+        # if len(nums) == 1:
+        #     return True
 
-        for i in range(len(nums) - 2 , -1 , -1):
+        # for i in range(len(nums) - 2 , -1 , -1):
 
-            if  not nums[i]  and jamp_zero:
+        #     if  not nums[i]  and jamp_zero:
+        #         jamp_zero = False
+        #         index = i            
+        #     else:
+        #         if nums[i] + i > index:
+        #             index = 0
+        #             jamp_zero = True
 
-                jamp_zero = False
-                index = i
-                print(index)
-            
-            else:
-                print(nums[i] + i , index)
-                if nums[i] + i > index:
-                    index = 0
-                    jamp_zero = True
+        # return jamp_zero
 
-        return jamp_zero
+        last = len(nums)-1
+
+        for i in range(len(nums)-2 , -1 ,-1):
+            if nums[i] + i >= last:
+                last = i
+        return last == 0
+
+
        
 
         
