36e250c29297f7b2d38a4e617cccf46366b6e561
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-09 07:23:48 +0300

    Time: 35 ms (81.49%) | Memory: 18 MB (81.65%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,34 +1,48 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l_dif=0
-        r_dif = 0
+        # l_dif=0
+        # r_dif = 0
+        # l = 0
+        # r = len(s) - 1
+
+        # while l<r:
+        #     if s[l] == s[r]:
+        #         l+=1
+        #         r -= 1
+        #     else:
+        #         l += 1
+        #         l_dif +=1
+
+        # if l_dif <= 1:       
+        #     return True
+
+        # l = 0
+        # r = len(s) - 1
+
+        # while l<r:
+        #     if s[l] == s[r]:
+        #         l+=1
+        #         r -= 1
+        #     else:
+        #         r -= 1
+        #         r_dif +=1
+        # if r_dif <= 1:       
+        #     return True
+        # return False
+
+
         l = 0
         r = len(s) - 1
 
-        while l<r:
-            if s[l] == s[r]:
-                l+=1
-                r -= 1
-            else:
-                l += 1
-                l_dif +=1
+        while l < r:
 
-        if l_dif <= 1:       
-            return True
-
-        l = 0
-        r = len(s) - 1
-
-        while l<r:
-            if s[l] == s[r]:
-                l+=1
-                r -= 1
-            else:
-                r -= 1
-                r_dif +=1
-        if r_dif <= 1:       
-            return True
-        return False
+            if s[r] != s[l] :
+                strR,strL =s[l:r], s[l+1:r+1]
+               
+                return strR[::-1]==strR or strL[::-1]==strL
+            l += 1
+            r -= 1
+        return True
         
         
         
