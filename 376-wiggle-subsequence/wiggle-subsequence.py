12e136095f2d56d7b6c5a09ba9bcfd5f01fd459d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-09 15:14:18 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (9.11%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.py
@@ -0,0 +1,55 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1
+       
+
+        i = 1
+        val = nums[1] - nums[0]
+        ans = 1
+
+        if val > 0 or val < 0:
+            ans  += 1
+        found = False
+
+        while i < len(nums):
+            found = False
+            
+           
+            if val > 0:
+                
+                while i < len(nums) and val >= 0 :
+                    if nums[i] - nums[i - 1] < 0:
+                        found = True
+                        ans += 1
+
+                    val = nums[i] - nums[i - 1]
+                    i += 1
+            elif val < 0:
+
+                while i < len(nums) and val <= 0 :
+                    if nums[i] - nums[i - 1] > 0:
+                        found = True
+                        ans += 1
+
+                    val = nums[i] - nums[i - 1]
+                    i += 1
+               
+
+            else :
+                
+
+                while i < len(nums) and val == 0 :
+
+                    if nums[i] - nums[i - 1] > 0 or nums[i] - nums[i - 1] < 0  :
+                        ans += 1
+
+                    val = nums[i] - nums[i - 1]
+                    i += 1
+        
+
+            
+            
+
+        return ans
+        
