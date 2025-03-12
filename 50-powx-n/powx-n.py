92ae9ad97225a64a3ad04a75115c265cc2d62a38
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-12 14:06:02 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (31.90%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n >= 0:
+            return self.solve(x,n)
+        else:
+            print(abs(1 / self.solve(x,abs(n))))
+            print(-1.0 if n % 2 and x < 0 else 1.0)
+            return  (abs(1 / self.solve(x,abs(n)))) * (-1.0 if n % 2 and x < 0 else 1.0)
+    
+
+    def solve(self, x , n ):
+        if n == 0:
+            return 1
+        
+        if n % 2 == 0 :
+            
+            return  self.solve(x * x ,n//2)
+
+        else:
+
+            return  x * self.solve(x,n-1)
+
+       
+        
