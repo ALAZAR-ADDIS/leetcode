58ca55f7f12a38d387193532a48508a7d01de21c
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-18 17:56:52 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.61%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -2,20 +2,39 @@ class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         
 
-        def solve(n,nums,val = ""):
-            if len(val) > n:
-                return 
+        # def solve(n,nums,val = ""):
+        #     if len(val) > n:
+        #         return 
 
-            if len(val) == n and val not in nums:
-                return  val
+        #     if len(val) == n and val not in nums:
+        #         return  val
             
-            val1 = solve(n,nums,val= val + "0")
-            val2 = solve(n,nums,val= val +"1")
+        #     val1 = solve(n,nums,val= val + "0")
+        #     val2 = solve(n,nums,val= val +"1")
 
-            if val1:
-                return val1
-            if val2:
+        #     if val1:
+        #         return val1
+        #     if val2:
+        #         return val2
+        # return solve(len(nums),set(nums),"")
+
+        def solve(i,arr):
+            if i == len(arr):
+                return None if "".join(arr) in numSet else  "".join(arr)
+            
+            val = solve(i + 1,arr)
+            if val:
+                return val
+
+            arr[i] = "1"
+            val2 = solve(i + 1, arr)
+            
+            if val2 :
                 return val2
-        return solve(len(nums),set(nums),"")
+
+
+        numSet = set(nums)
+        arr = ["0"] * len(nums)
+        return solve(0,arr)
 
         
