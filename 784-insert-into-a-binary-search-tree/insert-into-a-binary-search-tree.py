2bdecfe6c9079b22a493096c548456e135254b80
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-18 14:11:27 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (94.93%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -7,26 +7,42 @@
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
 
-        def helper(root,val):
+        if not root:
+            new = TreeNode(val)
+            return new
+            
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right,val)
+        
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left,val)
+
+        return root
+
+        # def helper(root,val):
+
+        #     if not root:
+        #         return TreeNode(val)
 
 
                       
-            if root.val <  val :
-                if not root.right:
-                    root.right = TreeNode(val)
-                    return 
-                helper( root.right ,val)
+        #     if root.val <  val :
+        #         if not root.right:
+        #             root.right = TreeNode(val)
+        #             return root
+        #         helper( root.right ,val)
                  
-            else:
-                if not root.left:
-                    root.left = TreeNode(val)
-                    return 
-                helper( root.left ,val)
-                 
-        if not root:
-            return TreeNode(val)
-        helper(root,val)
+        #     else:
+        #         if not root.left:
+        #             root.left = TreeNode(val)
+        #             return 
+        #         helper( root.left ,val)
 
-        return root
+                 
+        # if not root:
+        #     return TreeNode(val)
+        # helper(root,val)
+
+        # return root
 
         
