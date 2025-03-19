a400b9afb258ec50dca5633aeacdcdb8ea257f5f
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-19 14:53:17 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.18%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -8,23 +8,60 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
     
-        def solve(root1,root2):
+        # def solve(root1,root2):
 
-            if not root1 and not root2:
+        #     if not root1 and not root2:
+        #         return True
+
+        #     if (not root1 and root2) or (not root2 and root1):
+
+        #         return False
+
+        #     if root1.val != root2.val:               
+        #         return False
+
+        #     left = solve(root1.left, root2.right)
+        #     right = solve(root1.right,root2.left)
+
+
+        #     return left and right
+
+        # return  solve(root.left,root.right)
+
+        def solve(qu):
+            if not qu:
                 return True
 
-            if (not root1 and root2) or (not root2 and root1):
+            leng = len(qu)
+            while  leng > 0:
+                left = qu.popleft() if qu else None
+                right = qu.popleft() if qu else None
+                
+                
 
-                return False
+                if (left and not right) or (not left and right):
+                    return False
 
-            if root1.val != root2.val:               
-                return False
+                if left and right and left.val != right.val:
+                    return False
 
-            left = solve(root1.left, root2.right)
-            right = solve(root1.right,root2.left)
+                if left:
+                    qu.append(left.left)
+                if right:
+                    qu.append(right.right)
+                
+                if left:
+                    qu.append(left.right)
+                if right:
+                    qu.append(right.left) 
+                
+                leng -= 1
+        
+            return solve(qu)
+        q = deque()
+        q.append(root.left)
+        q.append(root.right)
+        return solve(q)
 
-
-            return left and right
-
-        return  solve(root.left,root.right)
+            
             
