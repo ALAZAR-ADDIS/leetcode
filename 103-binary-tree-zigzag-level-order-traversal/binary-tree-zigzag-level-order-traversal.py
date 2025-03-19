89b9dbe9904847d8d84a5ac513f8f7bef2c9adf6
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-19 07:02:22 +0300

    Time: 3 ms (7.32%) | Memory: 18.1 MB (49.81%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -15,20 +15,39 @@ class Solution:
             temp = []
 
             while leng > 0:
-                poped = qu.popleft()
-                temp.append(poped.val)
 
-                if poped.left:
-                    qu.append(poped.left)
-                
-                if poped.right:
-                    qu.append(poped.right)
+                if rev:
+
+                    poped = qu.pop()
+                    temp.append(poped.val)
+
+                    
+                    
+                    if poped.right:
+                        qu.appendleft(poped.right)
+                    
+                    if poped.left:
+                        qu.appendleft(poped.left)
+
+                    
+
+                else:
+                    poped = qu.popleft()
+                    temp.append(poped.val)
+
+                    if poped.left:
+                        qu.append(poped.left)
+                    
+                    if poped.right:
+                        qu.append(poped.right)
+               
+                    
                 leng -= 1
+            ans.append(temp)
+
             if rev:
-                ans.append(temp[::-1])
                 helper(q,False)
             else:
-                ans.append(temp)
                 helper(q,True)
             
         if not root:
