ce1baa3cb29593f3f1cc134fa9bc04c9fc76b5e0
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-20 06:00:57 +0300

    Time: 3 ms (35.62%) | Memory: 19.6 MB (89.89%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,20 +7,31 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        def solve(root):
+
+
+
+        def helper(root,start,end):
             if not root:
-                return
+                return True
             
-            solve(root.left)
-            stack.append(root.val)
-            solve(root.right)
-        stack = []
-        solve(root)
-        temp = stack[0]
-        for i  in range(1,len(stack)):
-            if stack[i] <= temp:
-                return False
-            temp = stack[i]
-        return True
+            return start < root.val < end and helper(root.left,start,root.val) and helper(root.right,root.val,end)
+        return helper(root,-float("inf"),float("inf"))
+
+        # def solve(root):
+        #     if not root:
+        #         return
+            
+        #     solve(root.left)
+        #     stack.append(root.val)
+        #     solve(root.right)
+
+        # stack = []
+        # solve(root)
+        # temp = stack[0]
+        # for i  in range(1,len(stack)):
+        #     if stack[i] <= temp:
+        #         return False
+        #     temp = stack[i]
+        # return True
         
         
