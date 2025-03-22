9f3fd677f397e1a6aedb1d2d61e74c0b003f67c8
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-22 05:17:05 +0300

    Time: 3 ms (32.69%) | Memory: 18.2 MB (14.96%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,23 +1,41 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        def helper(path,added:set):
-            if len(path) == len(nums):
-                ans.append(path[::])
-                return
+        # def helper(path,added:set):
+        #     if len(path) == len(nums):
+        #         ans.append(path[::])
+        #         return
             
 
-            for i in range(len(nums)):
+        #     for i in range(len(nums)):
 
-                if nums[i] not in added:
-                    path.append(nums[i])
-                    added.add(nums[i])
+        #         if nums[i] not in added:
+        #             path.append(nums[i])
+        #             added.add(nums[i])
 
-                    helper(path, added)
+        #             helper(path, added)
 
-                    path.pop()
-                    added.remove(nums[i])
+        #             path.pop()
+        #             added.remove(nums[i])
+     
+
+        def solve(i,nums):
+           
+            if i == len(nums):
+                print(nums)
+                ans.append(nums[::])
+                return
+            
+            for j in range(i,len(nums)):
+                nums[i] ,nums[j] = nums[j] ,nums[i]
+
+                solve(i + 1,nums)
+
+                nums[i] ,nums[j] = nums[j] ,nums[i]
+                
+
 
         ans = []
-        helper([],set())
+        solve(0,nums)
+
         return ans
