4ab96c90868d0d9457e4b4488b07481499f0b778
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-25 05:22:03 +0300

    Time: 3 ms (56.55%) | Memory: 19.3 MB (88.68%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -9,25 +9,26 @@ class Solution:
 
         def solve(root,maxx,minn):
 
-            nonlocal ans
             if not root:
-                return 
+                return  -float("inf")
             
-            ans = max(ans,abs(maxx - root.val),abs(minn- root.val))
+            
 
             maxx = max(maxx,root.val)
             minn = min(minn,root.val)
-            solve(root.left,maxx,minn)      
+
+            left = solve(root.left,maxx,minn)  
         
-            solve(root.right,maxx,minn)
-           
+            right =solve(root.right,maxx,minn)
 
-        ans = -float("inf")
+            return max(maxx- minn,left,right)   
+
+        
 
 
-        solve(root,root.val,root.val)
+       
 
-        return ans
+        return  solve(root,root.val,root.val)
 
         
    
