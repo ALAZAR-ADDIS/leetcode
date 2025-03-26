07b8e09a1f0628f42a9103e1cba4c77060e8f3bc
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-26 10:33:13 +0300

    Time: 3 ms (24.11%) | Memory: 17.7 MB (85.09%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,12 +1,42 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        stack , res = [], []
+        # stack , res = [], []
 
-        for i in range(len(pattern) + 1):
-            stack.append(i + 1)
+        # for i in range(len(pattern) + 1):
+        #     stack.append(i + 1)
 
-            while stack and (i== len(pattern) or pattern[i]=="I"):
-                res.append(str(stack.pop()))
-        return "".join(res)
+        #     while stack and (i== len(pattern) or pattern[i]=="I"):
+        #         res.append(str(stack.pop()))
+        # return "".join(res)
+
+
+        def solve(index,path):
+          
+            if len(path) == len(pattern) + 1: 
+                              
+                return "".join(map(str,path))
+            
+            for i in range(1,10):
+
+                if i in maped:
+                    continue
+              
+                if path and ((pattern[index -1]=="I" and path[-1] >= i) or (pattern[index - 1]=="D" and path[-1] <= i)):
+                    continue
+                
+                path.append(i)
+                maped.add(i)
+                
+                val = solve(index + 1,path)
+                
+                if val:
+                    return val
+               
+                path.pop()
+                maped.remove(i)
+        maped = set()
+        return solve(0,[])
+
+
             
        
