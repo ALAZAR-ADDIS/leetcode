3175ab8bb299fd87d47aa9a3d1d457f7e69c3801
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-26 22:08:19 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (69.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,24 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #if open=close=n return all the parantasis
-        #if open>close add closing paranthasis paranthasis
-        #if  opening is <n app opening paranthasis
-        ans,stack=[],[]
 
-        def backTrack(openB,closeB):
-            if openB==closeB==n:
-                ans.append("".join(stack))
+        def solve(op , cl,path):
+            if op == n  and cl == n :
+                ans.append("".join(path))
                 return 
             
-            if openB<n:
-                stack.append("(")
-                backTrack(openB+1,closeB)
-                stack.pop()
-            if openB>closeB:
-                stack.append(")")
-                backTrack(openB,closeB+1)
-                stack.pop()
-        backTrack(0,0)
+            if op < n:
+                path.append("(")
+                solve(op + 1, cl ,path)
+                path.pop()
+            
+            
+            if op > cl:
+                path.append(")")
+                solve(op, cl + 1,path)
+                path.pop()
+
+        ans = []
+        solve(0,0,[])
         return ans
-        
         
