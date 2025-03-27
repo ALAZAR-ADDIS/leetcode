15a22064f6b55fef0d1303d530673c28aee9db73
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-27 12:17:22 +0300

    Time: 1147 ms (17.11%) | Memory: 21.9 MB (84.04%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -0,0 +1,23 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+
+        def validator(time):
+            car = 0
+            for i in range(len(ranks)):
+                car += int(sqrt(time//ranks[i]))
+            return car
+        
+        l = 1
+        r = max(ranks) * cars* cars
+        
+
+        while l <= r:
+            mid = (l + r)//2
+
+            if validator(mid) >= cars:
+
+                r = mid - 1
+            else:
+
+                l = mid + 1
+        return l
