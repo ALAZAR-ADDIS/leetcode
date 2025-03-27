b38cd19cada85f537566fe2278b126328466d590
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-03-27 09:35:34 +0300

    Time: 13 ms (59.56%) | Memory: 18.2 MB (10.16%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
@@ -1,37 +1,76 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
         
-        def solve(i,path):
-            nonlocal maxx
+        # def solve(i,path):
+        #     nonlocal maxx
 
-            if i == len(words):
-                con = Counter(letters)
-                temp = 0
-                for wor in path:
-                    fre = Counter(wor)
+        #     if i == len(words):
+        #         con = Counter(letters)
+        #         temp = 0
+        #         for wor in path:
+        #             fre = Counter(wor)
 
-                    for f in fre:
+        #             for f in fre:
 
-                        if con[f] >= fre[f]:
-                            temp += score[ord(f) - ord("a")] * fre[f]
-                            con[f] -= fre[f]
-                        else:
-                            return 
+        #                 if con[f] >= fre[f]:
+        #                     temp += score[ord(f) - ord("a")] * fre[f]
+        #                     con[f] -= fre[f]
+        #                 else:
+        #                     return 
 
-                maxx = max(maxx,temp)
-                return
+        #         maxx = max(maxx,temp)
+        #         return
             
-            path.append(words[i])
-            solve(i + 1,path)
+        #     path.append(words[i])
+        #     solve(i + 1,path)
 
-            path.pop()
-            solve(i + 1,path)
+        #     path.pop()
+        #     solve(i + 1,path)
 
-        maxx = 0
-        solve(0,[])
+        # maxx = 0
+        # solve(0,[])
      
 
-        return maxx
+        # return maxx
+        def check(word):
+            count = Counter(word)
+            for  w in count:
+                if main[w] < count[w]:
+                    return False
+            return True
+        def calculate_score(word):
+            scor = 0
+
+            for w in word:
+                scor += score[ord(w) - ord("a")]
+
+            return scor
+
+        def solve(i):
+            if i == len(words):
+                return 0 
+            
+            res = solve(i +1)
+
+            if check(words[i]):
+
+                for j in words[i]:
+                    main[j] -= 1
+                
+                res = max(res, calculate_score(words[i]) + solve(i + 1))
+                
+                for j in words[i]:
+                    main[j] += 1
+                
+
+            return res
+
+
+
+        main = Counter(letters)
+        return solve(0)
+
+
             
 
         
