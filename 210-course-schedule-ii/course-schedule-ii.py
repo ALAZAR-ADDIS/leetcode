e574539c7b1d61d84fdc07413facd427b94ffe15
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-04-25 09:25:03 +0300

    Time: 7 ms (28.12%) | Memory: 19.5 MB (35.20%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,74 +1,77 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 
-        # def dfs(node):
-        #     if color[node] == 1:               
-        #         return False
+        def dfs(node):
+            if color[node] == 1:               
+                return False
             
-        #     color[node] = 1
+            color[node] = 1
 
             
 
-        #     for n in graph[node]:
-        #         if color[node] == 2:
-        #             continue
+            for n in graph[node]:
+                # print(n,color[3])
+                if color[n] == 2:
+                    continue
                 
-        #         if  not dfs(n):
-        #             return False
+                if  not dfs(n):
+                    return False
            
-        #     color[node] = 2                
-        #     order.append(node)
-        #     return True
+            color[node] = 2               
+            order.append(node)
+            return True
 
-        # graph = defaultdict(list)
-        # order = []
-        # color = defaultdict(int)
-
-        # for cor,pre in prerequisites:
-        #     graph[cor].append(pre)
-
-        # for i in range(numCourses):
-            
-
-        #     if color[i]:
-        #         continue
-       
-
-        #     if  not dfs(i) :
-        #         return []
-        # return order
-
-
-        qu = deque()
         graph = defaultdict(list)
-        indegree = defaultdict(int)
+        order = []
+        color = defaultdict(int)
 
         for cor,pre in prerequisites:
             graph[pre].append(cor)
-            indegree[cor] += 1
-        
+
         for i in range(numCourses):
-            if indegree[i] == 0:
-                qu.append(i)
+            
 
-        order = []
+            if color[i] :
+                print(i,"this is i")
+                continue
+
+       
+
+            if  not dfs(i) :
+                return []
+        return order[::-1]
+
+
+        # qu = deque()
+        # graph = defaultdict(list)
+        # indegree = defaultdict(int)
+
+        # for cor,pre in prerequisites:
+        #     graph[pre].append(cor)
+        #     indegree[cor] += 1
         
-        while qu:
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         qu.append(i)
 
-            node = qu.pop()
-            order.append(node)
+        # order = []
+        
+        # while qu:
+
+        #     node = qu.pop()
+        #     order.append(node)
             
             
 
-            for n in graph[node]:                
-                indegree[n] -= 1
+        #     for n in graph[node]:                
+        #         indegree[n] -= 1
 
-                if indegree[n] == 0:
-                    qu.append(n)
+        #         if indegree[n] == 0:
+        #             qu.append(n)
 
-        if len(order) != numCourses:
-            return  []
-        return order
+        # if len(order) != numCourses:
+        #     return  []
+        # return order
             
 
 
