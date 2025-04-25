03c02bd7ceed66c6ce15fb6b7fc5625594574c3f
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-04-25 16:31:42 +0300

    Time: 50 ms (63.94%) | Memory: 18.4 MB (33.80%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -1,46 +1,84 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
 
-        def dfs(node):
+        # def dfs(node):
 
-            visited.add(node)
-            no = 0
-            ed = 0
+        #     visited.add(node)
+        #     no = 0
+        #     ed = 0
 
 
-            for n in graph[node]:
+        #     for n in graph[node]:
                
-                if n not in visited:
-                    nod,e = dfs(n)
-                    no += nod
-                    ed += e
+        #         if n not in visited:
+        #             nod,e = dfs(n)
+        #             no += nod
+        #             ed += e
                 
-                if (node,n) not in edge  and (n,node) not in edge:
-                    edge.add((node,n))
-                    ed += 1
+        #         if (node,n) not in edge  and (n,node) not in edge:
+        #             edge.add((node,n))
+        #             ed += 1
                     
-            return [no + 1, ed]
+        #     return [no + 1, ed]
                     
         
-        visited = set()
-        edge = set()
-        graph = defaultdict(list)
-        count = 0
+        # visited = set()
+        # edge = set()
+        # graph = defaultdict(list)
+        # count = 0
        
-        for x,y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
+        # for x,y in edges:
+        #     graph[x].append(y)
+        #     graph[y].append(x)
       
         
         
-        for i  in range(n):
-            if i not in visited:
-                no,ed = dfs(i) 
+        # for i  in range(n):
+        #     if i not in visited:
+        #         no,ed = dfs(i) 
                       
-                if ed == (no * (no - 1))/2:                    
+        #         if ed == (no * (no - 1))/2:                    
+        #             count += 1
+        
+        # return count
+
+
+
+        def dfs(node,res):
+
+            visited.add(node)
+            res.append(node)           
+
+
+            for n in graph[node]:
+                if n not in visited:
+                    dfs(n,res)
+            
+            
+            return  res
+
+        graph = defaultdict(list)
+        visited = set()
+        count = 0
+        
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+       
+        
+        for i in range(n):
+            if i not in visited:
+               
+                res = dfs(i,[])
+                flag = True
+
+                for n in res:
+                    if len(res) - 1 != len(graph[n]):
+                        flag = False
+                
+                if flag:
                     count += 1
         
         return count
-
 
            
