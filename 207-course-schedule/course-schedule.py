0cc916d74b4e5330df77cb86ddeacbfac9f9e08e
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-04-25 12:10:00 +0300

    Time: 5 ms (58.63%) | Memory: 19.2 MB (61.35%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,20 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
-        def cycleDetector(child):
-            visited[child] = "gray"
+        # def cycleDetector(child):
+        #     visited[child] = "gray"
 
             
-            for n in graph[child]:
-                if n not in visited:
-                    val   = cycleDetector(n)
-                    if not val:
-                        return False
-                elif visited[n] == "gray":                   
-                    return False
+        #     for n in graph[child]:
+        #         if n not in visited:
+        #             val   = cycleDetector(n)
+        #             if not val:
+        #                 return False
+        #         elif visited[n] == "gray":                   
+        #             return False
                 
-            visited[child] = "black"
-            return True
+        #     visited[child] = "black"
+        #     return True
 
 
 
@@ -29,18 +29,53 @@ class Solution:
             #         return True
             # return False
 
+        # graph = defaultdict(list)
+        # visited = defaultdict(str)
+
+        # for cor,pre in prerequisites:
+        #         graph[pre].append(cor)
+
+        # key = list(graph.keys())
+        # for g in range(numCourses):
+        #         if not cycleDetector(g):
+        #             return False
+
+        # return True
+
+
+
+        indegree = defaultdict(int)
         graph = defaultdict(list)
-        visited = defaultdict(str)
+        qu =  deque()
 
-        for cor,pre in prerequisites:
-                graph[pre].append(cor)
+        for x,y in  prerequisites:
+            graph[y].append(x)
+            indegree[x] += 1
+        
+        for i in range(numCourses):            
+            if indegree[i] == 0:
+                qu.append(i)
 
-        key = list(graph.keys())
-        for g in range(numCourses):
-                if not cycleDetector(g):
-                    return False
+        count = 0       
 
-        return True
+
+        while qu :
+
+            node = qu.popleft()
+            count += 1
+
+            for n in graph[node]:
+                indegree[n] -= 1
+                
+                if indegree[n] == 0:
+                    
+                    qu.append(n)
+        print(count)
+        
+        if count == numCourses:
+            return True
+        return False
+            
 
 
 
