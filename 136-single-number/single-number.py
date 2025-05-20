7681f52b65b00c649856dff654754a94d23057ff
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-05-20 16:50:25 +0300

    Time: 3 ms (66.02%) | Memory: 19.4 MB (94.59%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        vals = Counter(nums)
+        # vals = Counter(nums)
 
-        for key in vals:
-            if vals[key] == 1:
-                return key
+        # for key in vals:
+        #     if vals[key] == 1:
+        #         return key
         
         val = 0
         for num in nums:
-            var ^= num
+            val ^= num
         return val
                 
         
