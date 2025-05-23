9927642a9026cb44213d332a23c53753f6c05b06
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-05-24 00:02:31 +0300

    Time: 47 ms (5.08%) | Memory: 18.5 MB (69.69%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,27 +1,39 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         
-        def solve(path,i):
-            if i >= len(s):
-                ans.append("".join(path[::]))
-                return 
+        # def solve(path,i):
+        #     if i >= len(s):
+        #         ans.append("".join(path[::]))
+        #         return 
             
 
-            if not s[i].isalpha():
-                path.append(s[i])                
-                solve(path,i + 1)
-                path.pop()
+        #     if not s[i].isalpha():
+        #         path.append(s[i])                
+        #         solve(path,i + 1)
+        #         path.pop()
 
-            else:
-                path.append(s[i].upper())
-                solve(path,i + 1)
-                path.pop()
+        #     else:
+        #         path.append(s[i].upper())
+        #         solve(path,i + 1)
+        #         path.pop()
 
-                path.append(s[i].lower())
-                solve(path, i + 1)
-                path.pop()
+        #         path.append(s[i].lower())
+        #         solve(path, i + 1)
+        #         path.pop()
 
+        # ans = []
+        # solve([],0)
+        # return ans
         ans = []
-        solve([],0)
-        return ans
+        for i in range(1 << len(s)):
+            temp = []
+
+            for j in range(len(s)):
+                if 1 & i >> j:
+                    temp.append(s[j].upper())
+                else:
+                    temp.append(s[j].lower())
+
+            ans.append("".join(temp))
+        return list(set(ans))
                 
