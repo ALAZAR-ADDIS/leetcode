6f822405d03b49ea1b576d1cd3f32cce86859f9e
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-09-02 23:59:26 +0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (98.21%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def maxD(root):
+            if not root:
+                return 0
+
+            left = maxD(root.left)
+            if  left == -1:
+                return -1
+
+            right =  maxD(root.right)
+            if  right == -1:
+                return -1
+           
+            if abs(right - left) > 1:
+                return -1
+            
+            return max(left, right) + 1
+
+
+       
+       
+        maxx = maxD(root)
+       
+        return maxx != -1
+        
