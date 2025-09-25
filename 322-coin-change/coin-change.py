6338a7a2ba24c9d52e274b916bfefef693bd6c6d
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-09-25 19:52:23 +0300

    Time: 1410 ms (8.28%) | Memory: 382.6 MB (5.00%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,35 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = defaultdict(int)
+
+        def dp(i,c):
+            state = (i,c)
+            if state in memo:
+                return memo[state]
+            if c == 0:
+                return 0
+
+            if i == 0:
+                if  c % coins[i]: 
+                    return float("inf")
+                return c // coins[i]
+
+            times = 0
+            minn =  dp(i - 1, c)
+           
+            
+            if c >= coins[i]:
+               
+                val  = dp(i,c - coins[i])
+                minn = min(minn, val + 1)
+               
+            memo[state] = minn
+
+            return minn
+        val = dp(len(coins) - 1, amount)
+        return val  if val != float("inf")  else -1
+        
+
+            
+
+        
