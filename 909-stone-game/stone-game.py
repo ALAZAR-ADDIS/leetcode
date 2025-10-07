18201f2a92bda792d5e4a410ce0fe6b7d44692c7
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-10-07 11:34:04 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.64%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+
+        # def dp(i,j,turn):
+        #     if i > j:
+        #         return False
+            
+        #     if turn:
+        #         return dp(i + 1)
+        
