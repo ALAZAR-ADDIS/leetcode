72e675d807fae01e4a67b2051ccd40fc362d4ac4
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-10-07 12:30:45 +0300

    Time: 15 ms (44.67%) | Memory: 21.2 MB (43.04%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,11 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
-
-        # def dp(i,j,turn):
-        #     if i > j:
-        #         return False
+        memo = defaultdict(bool)
+        def dp(i,j , summ):
+            if i > j:
+                return sum(piles) - summ < summ
             
-        #     if turn:
-        #         return dp(i + 1)
+            state = (i,j, summ)
+            if state in memo:
+                return memo[state]
+            
+            memo[state] =  dp(i + 2,j, summ + piles[i]) or dp(i, j - 2,summ + piles[j])
+            return memo[state]
+        
+        return dp(0, len(piles) - 1,0)
+
+      
         
