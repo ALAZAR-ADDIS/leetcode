8a28289b3a73b8b01dc620d54e3cc1419524e027
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-10-15 16:52:58 +0300

    Time: 7 ms (84.50%) | Memory: 18.1 MB (70.74%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -3,17 +3,24 @@ class Solution:
         words.sort()
         ans = ""
         dicts = set()
-        print(words)
-        for w in words:
-            if ans == "" and len(w) == 1:
-                ans = w
-            if w[:len(w) - 1] in dicts  and ans[:len(ans) - 1] != w[:len(w) - 1]  and len(w) > len(ans): 
-                           
-                ans = w
-            if len(w) == 1 or w[: len(w) - 1] in dicts:
+        dicts.add("")
+        
+        # for w in words:
+        #     if ans == "" and len(w) == 1:
+        #         ans = w
+
+        #     if w[:len(w) - 1] in dicts  and ans[:len(ans) - 1] != w[:len(w) - 1]  and len(w) > len(ans):                           
+        #         ans = w
+        #     if len(w) == 1 or w[: len(w) - 1] in dicts:
                 
                 
-                dicts.add(w)
+        #         dicts.add(w)
+
+        for word in words:
+            if word[:-1] in dicts:
+                dicts.add(word)
+                if len(word) > len(ans):
+                    ans = word
         return ans
             
         
