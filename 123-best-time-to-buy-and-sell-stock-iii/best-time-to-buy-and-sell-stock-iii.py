e5a3f831e190db71fe7412da2d6df9eafe7fc314
Alazar Addis <154592516+ALAZAR-ADDIS@users.noreply.github.com>
2025-10-18 21:59:53 +0300

    Time: 2047 ms (7.37%) | Memory: 561.2 MB (5.18%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -2,16 +2,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         memo = defaultdict(int)
         def dp(i,state,tran):
-
-            if i == len(prices) or tran > 2:
+            if i == len(prices) :
                 return 0
 
             s = (i,state,tran)
             if s in memo:
                 return memo[s]
 
-            if state:           
-                
+            if state:
+                if tran >= 2:
+                    memo[s] = 0
+                    return memo[s]                
              
                 memo[s] =  max(dp(i + 1,not state, tran + 1) - prices[i], dp(i + 1, state, tran))
             else:
